89baf532b1f1d32f06b6268ab9745548873f7d83
AnaBeatrizChaves <anabeatrizc76@gmail.com>
2023-09-03 13:32:14 -0300

    commit

---
+++ OperadoresLogicos/Exercicio02/questao06.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//6. Codifique um programa que lê um número inteiro. A seguir o programa deve imprimir uma mensagem para o usuário dizendo se o número digitado é par. Se o número não for par, o programa não deve fazer nada.
+
+main(){
+    int num;
+
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+        printf("O numero digitado e par!!");
+    }
+}
---
+++ OperadoresLogicos/Exercicio02/questao10.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/* 10. Calcule as raízes da equação de 2º grau.
+
+A variável a tem que ser diferente de zero. Caso seja igual, imprima a mensagem “Não e equação de segundo grau”. 
+
+• Se ∆ < 0, não existe real. Imprima a mensagem: Não existe raiz. 
+
+• Se ∆ = 0, existe uma raiz real. Imprima a raiz e a mensagem Raiz  única.
+
+• Se ∆ ≥ 0, imprima as duas raízes reais. 
+*/
+
+main(){
+
+}
