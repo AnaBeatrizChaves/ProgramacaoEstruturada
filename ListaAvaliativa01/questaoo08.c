0a9e9f124e6fdc922a192942d8cb7ec73749db5a
AnaBeatrizChaves <anabeatrizc76@gmail.com>
2023-09-25 17:15:37 -0300

    commit

--- ListaAvaliativa01/questaoo08.c
+++ ListaAvaliativa01/questaoo08.c
@@ -14,8 +14,8 @@ O final da leitura de dados se dará com a entrada de um salário negativo. (Use
 */
 
 main() {
-    float sal, somaSal, somaFilhos, mediaSal, mediaFilhos, maiorSal;
-    int filhos, registro = 1, sal_ate_100 = 1, porcentagem;
+    float sal, mediaSal = 0, mediaFilhos = 0, maiorSal = 0, porcentagem;
+    int filhos, registro = 1, cont = 0, contSalMenor100 = 0;
 
 
     do {
@@ -25,31 +25,28 @@ main() {
         printf("\nPreencha os campos abaixo\n");
         printf("Salario: R$ ");
         scanf("%f", &sal);
-        printf("Numero de Filhos: ");
-        scanf("%d", &filhos);
-        
-        somaSal += sal;
-        mediaSal = somaSal / registro;
+            if(sal >= 0){ 
+                registro++;
+                cont++;   
+                printf("Numero de Filhos: ");
+                scanf("%d", &filhos);
+                mediaFilhos += filhos;
+                mediaSal += sal;
+                if(sal > maiorSal){
+                    maiorSal = sal;
+                }
+                if(sal <= 100){
+                    contSalMenor100++;
+                }
+            }
 
-        somaFilhos += filhos;
-        mediaFilhos = somaFilhos / registro;
-
-        if (sal > maiorSal){
-            maiorSal = sal;
-        }
-
-        if (sal <= 100) {
-            sal_ate_100++;
-        }
-
-        porcentagem = (sal_ate_100 * 100) / registro;
-        
-        registro++;
-
-    }while (sal > 0);
+    }while (sal >= 0);
+        mediaFilhos /= cont;
+        mediaSal /= cont;
+        porcentagem = (contSalMenor100 * 100.0) / cont;
         printf("\n\n_______________ RESULTADOS ________________\n");
-        printf("Media do salario da populacao: %.2f", mediaSal);
-        printf("\nMedia do numero de filhos: %.2f", mediaFilhos);
-        printf("\nValor do maior salario: R$ %.2f", maiorSal);
-        printf("\nPercentual de pessoas com salario ate R$100,00: %.2f", porcentagem);
+        printf("Media salarial: %.2f", mediaSal);
+        printf("\nMedia de filhos: %.2f", mediaFilhos);
+        printf("\nMaior salario: R$ %.2f", maiorSal);
+        printf("\nPercentual de pessoas com salario ate R$100,00: %.2f %%", porcentagem);
 }
