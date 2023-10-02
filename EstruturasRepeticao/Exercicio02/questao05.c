32d8a7710d55f052f4fc00dc46f783fe5b1a94a3
AnaBeatrizChaves <anabeatrizc76@gmail.com>
2023-10-02 08:39:47 -0300

    commit

--- EstruturasRepeticao/Exercicio02/questao05.c
+++ EstruturasRepeticao/Exercicio02/questao05.c
@@ -18,5 +18,93 @@ Após receber todos os dados dos entrevistados, deverá ser gerado os seguintes 
 */
 
 main(){
-	
+	int idade, voto = 0, opcao, contadorvoto = 0, mv = 0, sk = 0, am = 0, dm = 0, ah = 0, sf = 0, votomaioridade = 0;
+    char nome[20], sexo[3], sexof[]="f";
+
+  do{
+    printf("\nDigite o nome do participante:\n");
+    fgets(nome,20, stdin);
+
+    printf("\nDigite a idade:\n");
+    scanf("%d",&idade);
+    fflush(stdin);
+
+    printf("Digite:\nm-para masculino\nf-para feminino\n");
+    fgets(sexo, 3, stdin);
+    fflush(stdin);
+
+    printf("Vote no candidato digitando o codigo de cada um:\n 1-Sam kerr- Australia\n 2-Alex morgan-EUA\n 3-Dzsenifer marozsan-Alemanha\n 4-Amandine henry-França\n 5- Marta vieira-Brasil\n");
+    scanf("%d",&voto);
+
+    switch(voto){
+      case 1:
+        contadorvoto++;
+        sk++;
+        if(strcmp(sexo, sexof)==0){
+          sf++;
+        }
+        
+        break;
+      case 2: 
+        contadorvoto++;
+        am++;
+        if(strcmp(sexo, sexof)==0){
+          sf++;
+        }
+        break;
+      case 3:
+        contadorvoto++;
+        dm++;
+        if(strcmp(sexo, sexof)==0){
+          sf++;
+        }
+        break;
+      case 4:
+        contadorvoto++;
+        ah++;
+        if(strcmp(sexo, sexof)==0){
+          sf++;
+        }
+        break;
+      case 5:
+        contadorvoto++;
+        mv++;
+        if(strcmp(sexo, sexof)==0){
+          sf++;
+        }
+
+        if(idade>=18){
+          votomaioridade++;
+            
+        }
+        break;
+    }
+
+    if(contadorvoto>50 && contadorvoto<300){
+      printf("caso deseje finalizar a votação digite 0:\n ");
+      scanf("%d",&opcao);
+    }else{
+      
+      opcao=1;
+    }
+      
+  }while(opcao==1);
+
+  printf("\nNumero de votos da Sam kerr:%d\n",sk);
+  printf("\nNumero de votos da Alex morgan:%d\n",am);
+  printf("\nNumero de votos da Dzsenifer marozsan:%d\n",dm);
+  printf("\nNumero de votos da Amandine henry:%d\n",ah);
+  printf("\nNumero de votos da Marta vieira:%d\n",mv);
+
+  if(sk>am && sk>dm && sk>ah && sk>mv){
+    printf("\nSam kerr é a jogadora mais votada");
+  }else if(am>sk && am>dm && am>ah && am>mv){
+    printf("\nAlex morgan é a jogadora mais votada");
+  }else if (dm>sk && dm>am && dm>ah && dm>mv){
+    printf("\nDzsenifer marozsan é a jogadora mais votada");
+  }else if (ah>sk && ah>am && ah>dm && ah>mv){
+    printf("\nAmandine henry é a jogadora mais votada");
+  }else if(mv>sk && mv>am && mv>dm && mv>ah){
+    printf("\nMarta vieira é a jogadora mais votada");
+  }
 }
--- EstruturasRepeticao/Exercicio02/questao07.c
+++ EstruturasRepeticao/Exercicio02/questao07.c
@@ -11,7 +11,40 @@ o peso médio das mulheres;
 e o peso médio do grupo
 */ 
 
-main(){
+main() {
+    int idade,contador=0, homens=0, mulheres=0;
+    char nome[20], sexo[3], sexom[]="m", sexof[]="f";
+    float altura, peso, alturaMulher=0, alturaHomem=0, alturaMediahm, pesoHomem=0, pesoMulher=0, pesoMediohm,mediaPesoH, mediaPesoM,mediaAlturaH, mediaAlturaM;
 
+  do{
+    printf("\nDigite o nome do participante:\n");
+    fgets(nome,20, stdin);
+
+    printf("\nDigite a idade:\n");
+    scanf("%d",&idade);
+
+    printf("Digite:\nm-para masculino\nf-para feminino\n");
+    fgets(sexo, 3, stdin);
+    fflush(stdin);
+
+    printf("\nDigite a altura:\n");
+    scanf("%f",&altura);
+
+    printf("\nDigite o peso:\n");
+    scanf("%f",&peso);
     
+    contador++;
+
+    if(strcmp(sexo, sexof)==0){
+      mulheres++;
+      pesoMulher+=peso;
+      alturaMulher+=altura;
+      
+    }else{
+      homens++;
+      pesoHomem+=peso;
+      alturaHomem+=altura;
+    }
 }
+}
+    
