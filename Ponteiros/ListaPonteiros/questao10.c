9b6063f02f6e3aa2c7ce117cda8cd2a9852d2482
AnaBeatrizChaves <anabeatrizc76@gmail.com>
2023-11-29 18:50:07 -0300

    commit

--- Ponteiros/ListaPonteiros/questao10.c
+++ Ponteiros/ListaPonteiros/questao10.c
@@ -6,6 +6,16 @@
 #include<string.h>
 
 main(){
+    int array[5];
 
-    
+    printf("Digite 5 valores inteiros:\n");
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &array[i]);
+    }
+
+    printf("Dobro de cada valor:\n");
+    for (int i = 0; i < 5; i++) {
+        int *ptr = &array[i];
+        printf("%d\n", (*ptr) * 2);
+    }
 }
